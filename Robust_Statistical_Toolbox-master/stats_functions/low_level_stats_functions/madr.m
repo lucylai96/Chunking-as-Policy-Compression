ed2e8c4a9f51c69ed89ed457ffe45dea64ff8f56
Lucy Lai <lucylai.lxl@gmail.com>
2021-09-22 13:31:47 -0400

    blah

--- Robust_Statistical_Toolbox-master/stats_functions/low_level_stats_functions/madr.m
+++
@@ -1,8 +0,0 @@
-function out=madr(x)
-
-% Computes the median absolute deviation to the median, 
-% adjusting by a factor for asymptotically normal consistency.
-% This is the formula used in the R version of mad, 
-% which is different from the Matlab version.
-
-out=1.4826.*median(abs(x-median(x)));
--- errorbarPosition.m
+++
@@ -1,7 +0,0 @@
-function x = errorbarPosition(b, sem)
-
-[ngroups, nbars] = size(sem);
-x = nan(nbars, ngroups);
-for i = 1:nbars
-    x(i,:) = b(i).XEndPoints;
-end
